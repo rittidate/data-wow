55127989a2300d1728fbf0565a4f6942a68ae0d8
Rittidate Kit <rittidate.kit@truedigital.com>
2024-10-19 20:59:50 +0700

    finish login;

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,24 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  helper_method :current_user
+  helper_method :user_signed_in?
+  before_action :authenticate_user
+
+  def current_user
+    @current_user ||= session[:user_id] && User.find_by(id: session[:user_id]) 
+  end
+
+  def user_signed_in?
+    !current_user.nil?
+  end
+
+  def authenticate_user
+    redirect_to new_session_path, flash: {danger: 'You must be signed in'} if current_user.nil?
+  end
+
+  def redirect_if_authenticated
+    redirect_to root_path, flash: { info: 'You are already logged in.'} if user_signed_in?
+  end
+
 end
